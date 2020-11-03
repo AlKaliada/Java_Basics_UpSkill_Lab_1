7d73211374df131b74d138dcaca5bb33bc68013f
AlKaliada <Aliaksei.Kaliada@gmail.com>
2020-11-03 11:12:57 +0300

    fixes bugs in decomposition

--- src/com/epam/kaliada/strings/regepressionstask1/SplitString.java
+++ src/com/epam/kaliada/strings/regepressionstask1/SplitString.java
@@ -2,7 +2,7 @@ package com.epam.kaliada.strings.regepressionstask1;
 
 public class SplitString {
     public String[] createsArrayOfParagraphs(String string){
-        return string.split("\\s+");
+        return string.split("\n\\s?");
     }
     public String[] createArrayOfSentences(String string){
         return string.split("[!?.]+\\s*");

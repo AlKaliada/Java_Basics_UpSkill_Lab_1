b7d115c01b896c9c4caf89b0bcc15cf684a0eb73
AlKaliada <Aliaksei.Kaliada@gmail.com>
2020-12-02 08:49:53 +0300

    oop task3

--- src/com/epam/kaliada/oop/task3/Calendar.java
+++ src/com/epam/kaliada/oop/task3/Calendar.java
@@ -19,6 +19,30 @@ public class Calendar {
         addWeekends(year);
     }
 
+    public List<Holiday> getHolidays() {
+        return holidays;
+    }
+
+    public void setHolidays(List<Holiday> holidays) {
+        this.holidays = holidays;
+    }
+
+    public List<LocalDate> getWeekends() {
+        return weekends;
+    }
+
+    public void setWeekends(List<LocalDate> weekends) {
+        this.weekends = weekends;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     private void addWeekends(int year){
         LocalDate localDate = LocalDate.of(year, 1, 1);
         while (localDate.isBefore(LocalDate.of(year + 1, 1,1))){
@@ -27,12 +51,75 @@ public class Calendar {
             }
             localDate = localDate.plusDays(1);
         }
-        for (LocalDate local : weekends) {
-            System.out.println(local);
+    }
+
+    public void removeHoliday(LocalDate localDate){
+        for (Holiday holiday : holidays) {
+            if (holiday.getLocalDate() == localDate){
+                holidays.remove(holiday);
+                break;
+            }
+        }
+    }
+
+    public void removeHoliday(String name){
+        for (Holiday holiday : holidays) {
+            if (holiday.getName().equals(name)){
+                holidays.remove(holiday);
+                break;
+            }
         }
     }
 
     public class Holiday{
+        private LocalDate localDate;
+        private String name;
 
+        public Holiday(LocalDate localDate, String name) {
+            this.localDate = localDate;
+            this.name = name;
+        }
+
+        public LocalDate getLocalDate() {
+            return localDate;
+        }
+
+        public void setLocalDate(LocalDate localDate) {
+            this.localDate = localDate;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void addHoliday(){
+            if (checkHoliday()){
+                System.out.println("The holiday on this day is already exist");
+                return;
+            }
+            holidays.add(this);
+        }
+        private boolean checkHoliday(){
+            boolean isHolidayExist = false;
+            for (Holiday holiday : holidays) {
+                if (holiday.getLocalDate() == this.getLocalDate()){
+                    isHolidayExist = true;
+                    break;
+                }
+            }
+            return isHolidayExist;
+        }
+
+        @Override
+        public String toString() {
+            return "Holiday{" +
+                    "localDate=" + localDate +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 }
--- src/com/epam/kaliada/oop/task3/Main.java
+++ src/com/epam/kaliada/oop/task3/Main.java
@@ -1,8 +1,17 @@
 package com.epam.kaliada.oop.task3;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Calendar calendar = new Calendar(2020);
+        LocalDate newYearDate = LocalDate.of(calendar.getYear(), 1, 1);
+        Calendar.Holiday newYear = calendar.new Holiday(newYearDate, "New Year");
+        newYear.addHoliday();
+        LocalDate birthdayDate = LocalDate.of(calendar.getYear(), 3, 15);
+        Calendar.Holiday birthday = calendar.new Holiday(birthdayDate, "Birthday");
+        birthday.addHoliday();
+        calendar.removeHoliday("Birthday");
 
     }
 }
